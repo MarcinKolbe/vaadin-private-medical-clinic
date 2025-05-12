3e02a47602832d7d7754fc424b0bec9262bdac5a
Marcin Kolbe <marcin.kolbe@gmail.com>
2025-05-12 19:25:15 +0200

    First commit

---
+++ src/main/java/com/rest/vaadin_private_medical_clinic/VaadinPrivateMedicalClinicApplication.java
@@ -0,0 +1,13 @@
+package com.rest.vaadin_private_medical_clinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VaadinPrivateMedicalClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VaadinPrivateMedicalClinicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rest/vaadin_private_medical_clinic/VaadinPrivateMedicalClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rest.vaadin_private_medical_clinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VaadinPrivateMedicalClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
